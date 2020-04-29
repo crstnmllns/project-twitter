22357c4c11e9d62bbf56fb6dc847169d469b218f
crstn <cristiancarrasco80@gmail.com>
2020-04-28 21:18:04 -0400

    create model and controller, create tweet and register user

---
+++ app/models/tweet.rb
@@ -0,0 +1,6 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  validates :content , presence: true
+  has_many :likes
+  has_many :reweets
+end
---
+++ db/migrate/20200429010439_add_retweet_ref_to_tweet.rb
@@ -0,0 +1,5 @@
+class AddRetweetRefToTweet < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :tweets, :retweet, foreign_key: true
+  end
+end
